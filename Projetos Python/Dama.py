d3377315c8ef4526a5cd146676f61517b515ee22
Meison de Jesus <meisonwilliams13@gmail.com>
2022-03-22 00:51:12 -0300

    Forma Tabular

--- Projetos Python/Dama.py
+++ Projetos Python/Dama.py
@@ -3,4 +3,8 @@ for iterator_1 in tabuleiro:
     tabuleiro[int(iterator_1)] = []
     for iterator_2 in range(0, 8):
         tabuleiro[int(iterator_1)].insert(int(iterator_2), "x")
-print(tabuleiro)
+        ("\n")
+for iterator_1 in tabuleiro:
+    for iterator_2 in iterator_1:
+        pass
+    print(iterator_1)
